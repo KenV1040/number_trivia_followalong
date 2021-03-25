6639af97c6d51f39aa9c84999b75df92031c5960
Ken <kenvu@protonmail.com>
2021-03-25 18:24:50 +0000

    Changed imports to relative imports

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,9 +1,10 @@
-import 'package:equatable/equatable.dart';
-import 'package:number_trivia2/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
-import 'package:number_trivia2/core/usecases/usecase.dart';
-import 'package:number_trivia2/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:number_trivia2/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
 
 class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
