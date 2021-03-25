8a2821df3696963bd8b2fa833a29036de6672679
Ken <kenvu@protonmail.com>
2021-03-25 13:11:53 +0000

    Wrote the full test for get_concrete_number_trivia

--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,7 +1,9 @@
+import 'package:dartz/dartz.dart';
 import 'package:mockito/mockito.dart';
-import 'package:number_trivia2/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia2/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia2/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:number_trivia2/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 // Mock the Number Trivia Repository
 class MockNumberTriviaRepository extends Mock implements NumberTriviaRepository {}
@@ -17,6 +19,32 @@ void main() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
     usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
   });
+
+  final tNumber = 1; // tNumber (or Test Number) is the number we try to get from repository
+  final tNumberTrivia = NumberTrivia(number: tNumber, text: 'test'); // NumberTrivia should be returned from the mockRepository after supplying it with a number 
+
+  test (
+    'Should get trivia for number from repository',
+    () async {
+      // arrange
+      when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+      .thenAnswer((_) async =>  Right(tNumberTrivia));
+      
+      // act
+      final result = await usecase.execute(number: tNumber);
+
+      // assert
+      expect(result, Right(tNumberTrivia));
+      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      // Ensures the tNumber variable was passed and not some hard coded number.
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
+      // Ensures no more interactions is done on the repository.  
+        
+      // Arrange is the theory part, where you say this is what should happen.
+      // Act is the actual action being performed
+      // Assert tells you that it expects the act result to match the assert result 
+    }
+  );
 }
 
 
