82a116c81a7dfc0d9d231c5ea7d721de21b0d15c
Ken <kenvu@protonmail.com>
2021-03-25 18:25:16 +0000

    Removed unused imports, added needed imports

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -2,8 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_trivia2/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:number_trivia2/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:number_trivia2/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia2/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
 // Mock the Number Trivia Repository
 class MockNumberTriviaRepository extends Mock implements NumberTriviaRepository {}
