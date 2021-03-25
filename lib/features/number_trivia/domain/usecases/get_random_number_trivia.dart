a917e5e0f294d1735fa4ed168927548947cca7da
Ken <kenvu@protonmail.com>
2021-03-25 18:24:01 +0000

    Added constructor argument to accept NumberTriviaRepository as an argument in this usecase

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -3,8 +3,13 @@ import 'package:number_trivia2/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:number_trivia2/core/usecases/usecase.dart';
 import 'package:number_trivia2/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia2/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
   @override
   Future<Either<Failure, NumberTrivia>> call(NoParams params) {
     // TODO: implement call
