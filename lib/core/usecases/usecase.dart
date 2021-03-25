2a40ea9fb57235cf642a9100d9dad5bc9f87adae
Ken <kenvu@protonmail.com>
2021-03-25 18:08:19 +0000

    Previous commit was wrong. This commit adds the usecase interface

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -2,6 +2,8 @@ import 'package:dartz/dartz.dart';
 
 import '../error/failures.dart';
 
+// Usecase interface. However, since dart doesn't have an interface, we use an abstract class instead.
+// This is essentially a contract that all usecases must have a call function implemented.
 abstract class UseCase <Type, Params> {
   // We don't currently need params. But this future-proofs the interface. Because if this
   // abstract function needs to do something, it won't be able to access the variables, so using params
